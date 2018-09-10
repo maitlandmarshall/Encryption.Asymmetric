fd60a6f007c0150e258c8cb3eb8d49f9f547e730
Maitland Marshall <maitlandmarshallapps@gmail.com>
2018-09-10 18:48:06 +1000

    cleaned up tests with dispose

--- Encryption.Asymmetric.Tests/RSAKeyPairTests.cs
+++ Encryption.Asymmetric.Tests/RSAKeyPairTests.cs
@@ -9,55 +9,58 @@ public class RSAKeyPairTests
     [TestMethod]
     public void NewKeyPair()
     {
-        RSAKeyPair kp = new RSAKeyPair();
+        using (RSAKeyPair kp = new RSAKeyPair())
+        using (RSAKeyPair kp2 = new RSAKeyPair())
+        {
+            Assert.AreNotEqual(new byte[0], kp.PrivateKey);
+            Assert.AreNotEqual(new byte[0], kp.PublicKey);
 
-        Assert.AreNotEqual(new byte[0], kp.PrivateKey);
-        Assert.AreNotEqual(new byte[0], kp.PublicKey);
-
-        RSAKeyPair kp2 = new RSAKeyPair();
-        CollectionAssert.AreNotEqual(kp2.PrivateKey, kp.PrivateKey);
-        CollectionAssert.AreNotEqual(kp2.PublicKey, kp.PublicKey);
+            CollectionAssert.AreNotEqual(kp2.PrivateKey, kp.PrivateKey);
+            CollectionAssert.AreNotEqual(kp2.PublicKey, kp.PublicKey);
+        }
     }
 
     [TestMethod]
     public void RestoreKeyPair()
     {
-        RSAKeyPair original = new RSAKeyPair();
-        RSAKeyPair restored = new RSAKeyPair(original.PrivateKey);
-
-        CollectionAssert.AreEqual(original.PrivateKey, restored.PrivateKey);
-        CollectionAssert.AreEqual(original.PublicKey, restored.PublicKey);
+        using (RSAKeyPair original = new RSAKeyPair())
+        using (RSAKeyPair restored = new RSAKeyPair(original.PrivateKey))
+        {
+            CollectionAssert.AreEqual(original.PrivateKey, restored.PrivateKey);
+            CollectionAssert.AreEqual(original.PublicKey, restored.PublicKey);
+        }
     }
 
     [TestMethod]
     public void Encrypt()
     {
-        RSAKeyPair kp = new RSAKeyPair();
+        using (RSAKeyPair kp = new RSAKeyPair())
+        {
+            string data = "Hello my name is Maitland";
+            byte[] dataEncrypted = kp.Encrypt(data, Encoding.UTF8);
+            string dataConvertedToUTF8 = Encoding.UTF8.GetString(dataEncrypted);
 
-        string data = "Hello my name is Maitland";
-        byte[] dataEncrypted = kp.Encrypt(data, Encoding.UTF8);
-        string dataConvertedToUTF8 = Encoding.UTF8.GetString(dataEncrypted);
-
-        Assert.AreNotEqual(data, dataConvertedToUTF8);
+            Assert.AreNotEqual(data, dataConvertedToUTF8);
+        }
     }
 
     [TestMethod]
     public void Decrypt()
     {
-        RSAKeyPair kp = new RSAKeyPair();
-
-        string data = "Hello I am Maitland I make code";
-
-        byte[] dataEncrypted = kp.Encrypt(data, Encoding.UTF8);
-        string dataDecrypted = kp.Decrypt(dataEncrypted, Encoding.UTF8);
-
-        Assert.AreEqual(data, dataDecrypted);
-
-        RSAKeyPair kp2 = new RSAKeyPair();
-        Assert.ThrowsException<CryptographicException>(() =>
+        using (RSAKeyPair kp = new RSAKeyPair())
+        using (RSAKeyPair kp2 = new RSAKeyPair())
         {
-            kp2.Decrypt(dataEncrypted, Encoding.UTF8);
-        });
+            string data = "Hello I am Maitland I make code";
+
+            byte[] dataEncrypted = kp.Encrypt(data, Encoding.UTF8);
+            string dataDecrypted = kp.Decrypt(dataEncrypted, Encoding.UTF8);
+
+            Assert.AreEqual(data, dataDecrypted);
+            Assert.ThrowsException<CryptographicException>(() =>
+            {
+                kp2.Decrypt(dataEncrypted, Encoding.UTF8);
+            });
+        }
     }
 
         
